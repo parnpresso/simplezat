aa0e32004307274cb156bdce642cd279b3349c40
parnpresso <parnpresso@gmail.com>
2018-12-08 17:16:55 +0700

    Update Cypress

--- tests/cypress/integration/simplezat/rating.spec.js
+++ tests/cypress/integration/simplezat/rating.spec.js
@@ -1,22 +1,22 @@
-context('Actions', () => {
+context("Actions", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8000/ratings')
-  })
+    cy.visit("http://localhost:8000/ratings");
+  });
 
-  it('should be able to give positive rating with comment', () => {
-    cy.contains('How do we do?')
-    cy.get('[href="/ratings/positive/"] > img').should('have.attr', 'alt', 'Positive')
-    cy.get('[href="/ratings/neutral/"] > img').should('have.attr', 'alt', 'Neutral')
-    cy.get('[href="/ratings/negative/"] > img').should('have.attr', 'alt', 'Negative')
+  it("should be able to give positive rating with comment", () => {
+    cy.contains("How do we do?");
+    cy.get('[href="positive/"] > img').should("have.attr", "alt", "Positive");
+    cy.get('[href="neutral/"] > img').should("have.attr", "alt", "Neutral");
+    cy.get('[href="negative/"] > img').should("have.attr", "alt", "Negative");
 
-    cy.get('[href="/ratings/positive/"] > img').click()
-    cy.wait(1000)
+    cy.get('[href="positive/"] > img').click();
+    cy.wait(1000);
 
-    cy.contains('Any comment?')
-    cy.get('input[name="comment"]').type('You are doing great!')
-    cy.get('button').click()
-    cy.wait(1000)
+    cy.contains("Any comment?");
+    cy.get("textarea").type("You are doing great!");
+    cy.get('[type="submit"]').click();
+    cy.wait(1000);
 
-    cy.contains('Thank you')
-  })
-})
+    cy.contains("Thank you");
+  });
+});
