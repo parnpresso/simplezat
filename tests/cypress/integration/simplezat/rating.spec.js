278db2f62951c3aa841e2676d1471c96ed7f1ea5
parnpresso <parnpresso@gmail.com>
2018-12-08 12:19:06 +0700

    Add wait stage after click

--- tests/cypress/integration/simplezat/rating.spec.js
+++ tests/cypress/integration/simplezat/rating.spec.js
@@ -10,11 +10,13 @@ context('Actions', () => {
     cy.get('img').should('have.attr', 'attr', 'Negative')
 
     cy.get('img[alt="Positive"]').click()
+    cy.wait(1000)
 
     cy.contains('Any comment?')
     cy.get('input[name="comment"]').type('You are doing great!')
     cy.get('button').click()
+    cy.wait(1000)
 
     cy.contains('Thank you')
-  }
+  })
 })
