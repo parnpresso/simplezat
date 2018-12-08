d00374e9074f4094a6377e8bf5ad92b8c9bde082
parnpresso <parnpresso@gmail.com>
2018-12-08 17:56:40 +0700

    Reorder URL rule

--- simplezat/ratings/urls.py
+++ simplezat/ratings/urls.py
@@ -4,7 +4,7 @@ from .views import CommentView, RatingView, ThankView
 
 
 urlpatterns = [
+    path('thanks/', ThankView.as_view(), name='thanks'),
     path('', RatingView.as_view(), name='ratings'),
     path('<str:rating>/', CommentView.as_view(), name='comments'),
-    path('thanks', ThankView.as_view(), name='thanks')
 ]
