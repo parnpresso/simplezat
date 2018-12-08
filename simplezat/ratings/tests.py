72fe6e8fc582de68994db3824f016921e5d2fb10
parnpresso <parnpresso@gmail.com>
2018-12-08 14:52:17 +0700

    Add text to rating page

--- simplezat/ratings/tests.py
+++ simplezat/ratings/tests.py
@@ -3,9 +3,17 @@ from django.test import TestCase
 
 
 class RatingViewTest(TestCase):
-	def test_rating_view_should_be_accessible(self):
-		url = reverse('ratings')
-		response = self.client.get(url)
+    def setUp(self):
+        self.url = reverse('ratings')
 
-		self.assertEqual(response.status_code, 200)
+    def test_rating_view_should_be_accessible(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_rating_view_should_have_question_text(self):    
+        response = self.client.get(self.url)
+
+        expected = '<h1>How do we do?</h1>'
+        self.assertContains(response, expected, status_code=200)
 
